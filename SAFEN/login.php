18752dfb94d9980eff0c6efd27be6616f7d290ca
diegorllq <rosalesdiego008@gmail.com>
2026-05-06 10:53:28 -0600

    error css

--- SAFEN/login.php
+++ SAFEN/login.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Forms</title>
-     <link rel="stylesheet" href="stylelog.css">
+     <link rel="stylesheet" href="/SAFEN/css/stylelog.css">
 </head>
 <body>
 
