e950aab388cbb3e11405b26639169101b24fbff4
diegorllq <rosalesdiego008@gmail.com>
2026-05-07 09:33:52 -0600

    prueba guardar cambios

--- SAFEN/perfil.php
+++ SAFEN/perfil.php
@@ -13,7 +13,47 @@ $sql = "SELECT * FROM usuarios WHERE nombre='$nombre'";
 $resultado = mysqli_query($conn, $sql);
 
 $usuario = mysqli_fetch_assoc($resultado);
+
+if (isset($_POST["guardar"])) {
+
+    $nuevoNombre = $_POST["nombre"];
+    $nuevoCorreo = $_POST["correo"];
+    $telefono = $_POST["telefono"];
+
+    $fotoNombre = $_FILES["foto"]["name"];
+    $rutaTemporal = $_FILES["foto"]["tmp_name"];
+
+    // Si subió imagen
+    if (!empty($fotoNombre)) {
+
+        // nombre único
+        $fotoFinal = time() . "_" . $fotoNombre;
+
+        move_uploaded_file($rutaTemporal, "img/" . $fotoFinal);
+
+        $sql = "UPDATE usuarios 
+                SET nombre='$nuevoNombre', correo='$nuevoCorreo', telefono='$telefono', foto='$fotoFinal'
+                WHERE nombre='$nombre'";
+    } else {
+
+        $sql = "UPDATE usuarios 
+                SET nombre='$nuevoNombre', correo='$nuevoCorreo', telefono='$telefono'
+                WHERE nombre='$nombre'";
+    }
+
+    if (mysqli_query($conn, $sql)) {
+
+        $_SESSION["usuario"] = $nuevoNombre;
+
+        header("Location: perfil.php");
+        exit();
+
+    } else {
+        echo "<script>alert('Error al actualizar');</script>";
+    }
+}
 ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -25,7 +65,11 @@ $usuario = mysqli_fetch_assoc($resultado);
 <body>
     <div class="perfil-container">
      <div class="left">
-        <img src="https://ui-avatars.com/api/?name=<?php echo $usuario['nombre']; ?>&background=15173d&color=fff" class="foto">
+        <?php if ($usuario["foto"]) { ?>
+    <img src="img/<?php echo $usuario["foto"]; ?>" class="foto">
+<?php } else { ?>
+    <img src="https://ui-avatars.com/api/?name=<?php echo urlencode($usuario['nombre']); ?>&background=15173d&color=fff" class="foto">
+<?php } ?>
      <H2>
         <?php echo $usuario["nombre"]; ?>
      </H2>
@@ -37,7 +81,7 @@ $usuario = mysqli_fetch_assoc($resultado);
     <div class="right">
 <h1>Ajustes</h1>
 
-<form method="POST">
+<form method="POST" enctype="multipart/form-data">
     <label>Nombre</label>
 <input type="text"
 name="nombre"
@@ -54,6 +98,10 @@ value="<?php echo $usuario["correo"]; ?>">
 <input type="text"
 name="telefono"
 value="<?php echo $usuario["telefono"]; ?>">
+
+<label>Foto de perfil</label>
+<input type="file"
+name="foto">
 
 <div class="btns">
     <button type="submit" name="guardar" class="btn-guardar">
