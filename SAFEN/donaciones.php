ffb7deb652e37cea5abc526553ee18669dca8073
diegorllq <rosalesdiego008@gmail.com>
2026-05-10 14:30:58 -0600

    donaciones inicio

---
+++ SAFEN/donaciones.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="/SAFEN/css/donaciones.css">
+    <link rel="stylesheet" href="/SAFEN/css/navbar.css">
+</head>
+<body>
+    <header class="header">
+        <?php include("navbar.php"); ?>
+<div class="hero">
+  <img src="/SAFEN/img/image (1).png" alt="imagen">
+
+  <!-- Curvas más altas pero rectas -->
+  <svg class="waves" viewBox="0 0 1440 180" preserveAspectRatio="none">
+    <path fill="#ffffff" d="
+      M0,90
+      C120,170 240,10 360,90
+      C480,170 600,10 720,90
+      C840,170 960,10 1080,90
+      C1200,170 1320,10 1440,90
+      L1440,180
+      L0,180
+      Z">
+    </path>
+  </svg>
+</div>
+
+</header>
+
+    </header>
+</body>
+</html>
