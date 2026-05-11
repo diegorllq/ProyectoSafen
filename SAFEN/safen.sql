60a4eefcf762009538c71e5dff8a3b3ea2c8083a
diegorllq <rosalesdiego008@gmail.com>
2026-05-10 22:17:43 -0600

    SUBIDA COPIA DE SEGURIDAD SAFEN

--- SAFEN/safen.sql
+++ SAFEN/safen.sql
@@ -2,8 +2,8 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Servidor: 127.0.0.1
--- Tiempo de generación: 08-05-2026 a las 22:49:45
+-- Servidor: 127.0.0.1:3307
+-- Tiempo de generación: 11-05-2026 a las 06:12:09
 -- Versión del servidor: 10.4.32-MariaDB
 -- Versión de PHP: 8.2.12
 
@@ -24,51 +24,100 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `donaciones`
+--
+
+CREATE TABLE `donaciones` (
+  `id` int(11) NOT NULL,
+  `usuario_id` int(11) DEFAULT NULL,
+  `monto` decimal(10,2) NOT NULL,
+  `metodo_pago` varchar(50) DEFAULT NULL,
+  `fecha` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Volcado de datos para la tabla `donaciones`
+--
+
+INSERT INTO `donaciones` (`id`, `usuario_id`, `monto`, `metodo_pago`, `fecha`) VALUES
+(1, 2, 100.00, 'paypal', '2026-05-10 22:09:53'),
+(2, 2, 100.00, 'paypal', '2026-05-10 22:19:26'),
+(3, 2, 100.00, 'paypal', '2026-05-10 22:23:35'),
+(4, 2, 100.00, 'paypal', '2026-05-10 22:27:29'),
+(5, 2, 250.00, 'paypal', '2026-05-11 03:12:45'),
+(6, 2, 250.50, 'paypal', '2026-05-11 04:06:27'),
+(7, 2, 1500.25, 'tarjeta', '2026-05-11 04:07:04');
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `usuarios`
 --
 
 CREATE TABLE `usuarios` (
   `id` int(11) NOT NULL,
-  `nombre` varchar(100) NOT NULL,
-  `correo` varchar(100) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  `telefono` varchar(20) NOT NULL,
-  `foto` varchar(255) NOT NULL
+  `nombre` varchar(100) DEFAULT NULL,
+  `correo` varchar(100) DEFAULT NULL,
+  `password` varchar(255) DEFAULT NULL,
+  `telefono` varchar(20) DEFAULT NULL,
+  `foto` varchar(255) DEFAULT NULL,
+  `reset_token` varchar(255) DEFAULT NULL,
+  `token_expira` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Volcado de datos para la tabla `usuarios`
 --
 
-INSERT INTO `usuarios` (`id`, `nombre`, `correo`, `password`, `telefono`, `foto`) VALUES
-(1, 'Diegoqrsll', 'diego.prueba@gmail.com', '$2y$10$AootRMvL3Cfyz8ThQ3ouqeexC6ETwrx6DvYayvyzJRNurfwr5yeRe', '', ''),
-(2, 'diegoquintanilla', 'rosalesdiego008@gmail.com', '$2y$10$4L0EZzHlWpoXdPTO4FsMRe8yFAfnpHrcWbXnc1dtd.aTEU2kR21LS', '', ''),
-(3, 'nosee', 'pruebaogin@gmail.com', '$2y$10$DiCtD4UZ6GPEPVWMfh1JLuMLvn2/6SR2fMhYdyljxoXt0qMTmWpmq', '', ''),
-(4, 'nouuu', 'prueba2@gmail.com', '$2y$10$o6i/ckGTCBg7WADcQNSNm.FO8tNXSQrrE1yGhAxdoxr3XkHMcZzya', '', ''),
-(5, 'maxrosalees', 'maxrosalees2@gmail.com', '$2y$10$KtgrFZzVRxvlbS/hwEuYAeJh24dDh2nNbPDUKx1SyjdZUZhG8./.m', '+503 7596-0942', '1778259116_gato-meme.gif'),
-(6, 'Marjorie', 'mar@gmail.com', '$2y$10$vC8J1p0as6mmETmert6yoO1nI1qmF6COUqXNVR8nuLkzmN9JGk3XC', '', ''),
-(7, 'quintanilla', 'quintanilla@gmail.com', '$2y$10$10dnTSPrg0BnZxGAqOu29eElmq49Ri80e2LH/ojXHSphD8mCidmXO', '', ''),
-(8, 'diegooquintanilla', 'diegoq@gmail.com', '$2y$10$l4Jpl7kDIFySY1mKjpkWVe8x461EA85QnMh.q8JfrJqbCWbODuZ.e', '', '1778262054_a914bd466e8986150e2ff3eb6eca4c5b.jpg');
+INSERT INTO `usuarios` (`id`, `nombre`, `correo`, `password`, `telefono`, `foto`, `reset_token`, `token_expira`) VALUES
+(1, 'diegoo7', 'diego8@gmail.com', '$2y$10$eUSWvO9KzQ/w4ZM.U4tFkugyoA4ioqSpcYkL1eYNNBnzfHfurtrkW', '', '1778302436_4c2dcd2825135aa4fa1104e5d5beb625.jpg', NULL, NULL),
+(2, 'diegorsl', 'rosalesdiego008@gmail.com', '$2y$10$zcmANuvU9fR.oq1sw0SdjeqbhIKPD5UvNhyzi5qPz66LpRQb3GO3i', '', NULL, NULL, NULL),
+(3, 'maxxrosales', 'maxrosales2@gmail.com', '$2y$10$Ko8vYBIIVljc67C.FCF9X.jAyeYJDmGz9.3S.CKOroopWXHtQ9s9e', NULL, NULL, NULL, NULL),
+(4, 'Diego022d', 'diegoalejandroquintanillarosal@gmailcom', '$2y$10$sUBEPZQ18u7bLJgTztbVS.50CwZxdeQcK9whW6HqDe5WYts0NnSmi', '', '1778419899_4c2dcd2825135aa4fa1104e5d5beb625.jpg', NULL, NULL);
 
 --
 -- Índices para tablas volcadas
 --
 
 --
+-- Indices de la tabla `donaciones`
+--
+ALTER TABLE `donaciones`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `usuario_id` (`usuario_id`);
+
+--
 -- Indices de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `correo` (`correo`);
 
 --
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
 --
+-- AUTO_INCREMENT de la tabla `donaciones`
+--
+ALTER TABLE `donaciones`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
 -- AUTO_INCREMENT de la tabla `usuarios`
 --
 ALTER TABLE `usuarios`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `donaciones`
+--
+ALTER TABLE `donaciones`
+  ADD CONSTRAINT `donaciones_ibfk_1` FOREIGN KEY (`usuario_id`) REFERENCES `usuarios` (`id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
