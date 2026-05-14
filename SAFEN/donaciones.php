e77db6a85f692f5f34a238c1c84f63bbb4984790
diegorllq <rosalesdiego008@gmail.com>
2026-05-14 14:52:37 -0600

    css final donaciones

--- SAFEN/donaciones.php
+++ SAFEN/donaciones.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Donaciones</title>
     <link rel="stylesheet" href="/SAFEN/css/donaciones.css">
     <link rel="stylesheet" href="/SAFEN/css/navbar.css">
 </head>
