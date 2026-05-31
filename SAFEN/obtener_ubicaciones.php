7fc6ec95b8a0fb9d01037fbe00f000e9dff7eaec
diegorllq <rosalesdiego008@gmail.com>
2026-05-30 22:21:41 -0600

    prueba 6 db mapas

--- SAFEN/obtener_ubicaciones.php
+++ SAFEN/obtener_ubicaciones.php
@@ -1,19 +1,23 @@
 <?php
+header('Content-Type: application/json');
+
 include("conexion.php");
 
-$resultado = $conexion->query("SELECT * FROM ubicaciones ORDER BY created_at DESC");
+$resultado = $conexion->query("SELECT * FROM ubicaciones ORDER BY id DESC");
 
 $ubicaciones = [];
 
-while($fila = $resultado->fetch_assoc()){
-    $ubicaciones[] = [
-        "id" => $fila['id'],
-        "name" => $fila['nombre'],
-        "desc" => $fila['descripcion'],
-        "lat" => (float)$fila['lat'],
-        "lng" => (float)$fila['lng'],
-        "color" => $fila['color']
-    ];
+if ($resultado) {
+    while($fila = $resultado->fetch_assoc()){
+        $ubicaciones[] = [
+            "id" => $fila['id'],
+            "name" => $fila['nombre'],
+            "desc" => $fila['descripcion'],
+            "lat" => (float)$fila['lat'],
+            "lng" => (float)$fila['lng'],
+            "color" => $fila['color']
+        ];
+    }
 }
 
 echo json_encode($ubicaciones);
